9dc949f55f3ba0c6ccd22a0ba42c0a5503b53329
teooko <teobogdann@yahoo.com>
2024-01-14 19:47:57 +0200

    Add basic timer

--- src/pages/Timer/index.jsx
+++ src/pages/Timer/index.jsx
@@ -1,17 +1,57 @@
 ﻿import {Button, Text, View} from "react-native";
 import Page from "../Page";
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {StyleSheet} from "react-native";
 
 function Timer({ navigation }) {
+    const [started, setStarted] = useState(false);
+    const [minutes, setMinutes] = useState(2);
+    const [seconds, setSeconds] = useState(0);
+    
+    useEffect(() => {
+        let timer;
+        if (started)
+            timer = setInterval(() => {
+                if (minutes > 0) {
+                        if (seconds === 0) {
+                            setSeconds(59);
+                            setMinutes(minutes - 1);
+                        } else {
+                            setSeconds(seconds - 1);
+                        }
+                        
+                }
+                else if (seconds > 0) {
+                        setSeconds(seconds - 1);
+                }
+                else {
+                    setStarted(false);
+                    clearInterval(timer);
+                }
+            }, 1000);
+        
+        return () => clearInterval(timer);
+    }, [started, minutes, seconds]);
     return (
         <View>
             <Page navigation={navigation}>
-                <Text>
-                    TIMEEER
+                <Text style={styles.timer}>
+                    {minutes > 9 ? minutes : "0" + minutes} : {seconds > 9 ? seconds : "0" + seconds}
                 </Text>
+                <Button title={started ? "stop" : "start"} onPress={() => setStarted(!started)}/>
             </Page>
         </View>
     );
 }
 
+const styles = StyleSheet.create({
+    timer: {
+        fontSize: 80,
+        color: "white",
+        marginTop: "auto",
+        marginBottom: "auto",
+        marginLeft: "auto",
+        marginRight: "auto",
+    }
+})
 export default Timer
