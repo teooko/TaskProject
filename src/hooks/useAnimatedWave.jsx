cd9d33322166ca4096dc9ed563fb2063d8d1a107
teooko <teobogdann@yahoo.com>
2024-03-13 18:28:15 +0200

    Fix syntax errors

--- src/hooks/useAnimatedWave.jsx
+++ src/hooks/useAnimatedWave.jsx
@@ -1,50 +1,50 @@
 ﻿import {
-  cancelAnimation,
-  Easing,
-  ReduceMotion,
-  useAnimatedProps,
-  useSharedValue,
-  withRepeat,
-  withSpring,
-  withTiming,
+    cancelAnimation,
+    Easing,
+    ReduceMotion,
+    useAnimatedProps,
+    useSharedValue,
+    withRepeat,
+    withSpring,
+    withTiming,
 } from 'react-native-reanimated';
 
 const useAnimatedWave = (initialValue, toValue, duration) => {
-  const offsetX = useSharedValue(initialValue);
-  const offsetY = useSharedValue(20);
+    const offsetX = useSharedValue(initialValue);
+    const offsetY = useSharedValue(20);
 
-  const animatedStyle = useAnimatedProps(() => ({
-    transform: [{translateX: offsetX.value}, {translateY: offsetY.value}],
-  }));
+    const animatedStyle = useAnimatedProps(() => ({
+        transform: [{translateX: offsetX.value}, {translateY: offsetY.value}],
+    }));
 
-  const startAnimation = () => {
-    offsetX.value = 0;
-    offsetX.value = withRepeat(
-      withTiming(toValue, {
-        duration: duration,
-        easing: Easing.inOut(Easing.linear),
-      }),
-      0,
-    );
-    offsetY.value = withRepeat(
-      withSpring(0, {
-        mass: 1,
-        damping: 8,
-        stiffness: 120,
-      }),
-      1,
+    const startAnimation = () => {
+        offsetX.value = 0;
+        offsetX.value = withRepeat(
+            withTiming(toValue, {
+                duration: duration,
+                easing: Easing.inOut(Easing.linear),
+            }),
+            0,
+        );
+        offsetY.value = withRepeat(
+            withSpring(0, {
+                mass: 1,
+                damping: 8,
+                stiffness: 120,
+            }),
+            1,
+        );
+    };
 
-  };
+    const stopAnimation = () => {
+        offsetY.value = withTiming(20, {
+            mass: 1,
+            damping: 2,
+            stiffness: 50,
+        });
+    };
 
-  const stopAnimation = () => {
-    offsetY.value = withTiming(20, {
-      mass: 1,
-      damping: 2,
-      stiffness: 50,
-    });
-  };
-
-return [animatedStyle, startAnimation, stopAnimation];
+    return [animatedStyle, startAnimation, stopAnimation];
 };
 
 export default useAnimatedWave;
