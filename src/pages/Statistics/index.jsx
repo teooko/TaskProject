c7f6f48f70928fea663026cdba9844f046d58867
teooko <teobogdann@yahoo.com>
2024-04-10 18:28:57 +0300

    Add task select list in the statistics page

--- src/pages/Statistics/index.jsx
+++ src/pages/Statistics/index.jsx
@@ -2,11 +2,21 @@
 import Page from "../Page";
 import {Dimensions, View, Text} from "react-native";
 import {LineChart} from "react-native-chart-kit";
+import {SelectList} from "react-native-dropdown-select-list/index";
+import {useSelector} from "react-redux";
 
 const Statistics = ({navigation}) => {
+    const {tasks} = useSelector(state => state.tasks);
     return (
         <Page navigation={navigation}>
-            <View>
+            <View style={{position: "absolute", top: 40, left: 30, width: 300, zIndex: 1}}>
+                <SelectList dropdownItemStyles={{backgroundColor: "#B83838"}}
+                            boxStyles={{backgroundColor: "#B83838", borderWidth: 0}}
+                            dropdownStyles={{backgroundColor: "#B83838", borderColor: "#560D0D"}}
+                            data={tasks.map(task => ({ key: task.id, value: task.name }))}
+                            setSelected={(val) => console.log(val)}/>
+            </View>
+            <View style={{marginTop: 70}}>
                 <Text>Bezier Line Chart</Text>
                 <LineChart
                     data={{
