3c715de0bcbda5e4778801cb9c1b9f0a0c427070
teooko <teobogdann@yahoo.com>
2024-04-11 21:51:29 +0300

    Extract total task time chart into its own component

---
+++ src/pages/Statistics/TotalTaskTimeChart.jsx
@@ -0,0 +1,80 @@
+﻿import React, {useEffect, useState} from 'react';
+import {Dimensions} from "react-native";
+import {BarChart} from "react-native-chart-kit";
+import {useDispatch, useSelector} from "react-redux";
+import {fetchTotalTasksTime} from "../../store/tasksSlice";
+
+//extract this function to its own file
+function convertTimeStringToHours(timeString) {
+    // Split the time string into components
+    const [hours, minutes, seconds] = timeString.split(':');
+
+    // Parse hours, minutes, and seconds
+    const parsedHours = parseInt(hours, 10);
+    const parsedMinutes = parseInt(minutes, 10);
+    const parsedSeconds = parseFloat(seconds); // Parse seconds as float for decimal precision
+
+    // Calculate total hours
+    const totalHours = parsedHours + (parsedMinutes / 60) + (parsedSeconds / 3600);
+
+    return totalHours;
+}
+
+const TotalTaskTimeChart = () => {
+    const dispatch = useDispatch();
+    const [data, setData] = useState({
+        labels: ["January", "February", "March", "April", "May"],
+        datasets: [
+            {
+                data: [20, 45, 28, 80, 99]
+            }
+        ]
+    });
+    const {totalTasksTime} = useSelector(state => state.tasks);
+
+    useEffect(() => {
+        dispatch(fetchTotalTasksTime());
+    }, [])
+
+    useEffect(() => {
+        const labels = totalTasksTime.map(task => task.name);
+        const data = totalTasksTime.map(task => convertTimeStringToHours(task.time));
+        const newData = {
+            labels: labels,
+            datasets: [
+                {
+                    data: data
+                }
+            ]
+        };
+        setData(newData);
+    }, [totalTasksTime]);
+    
+    return (
+        <BarChart
+            data={data}
+            width={Dimensions.get("window").width}
+            height={220}
+            yAxisSuffix="h"
+            chartConfig={{
+                backgroundColor: "#e26a00",
+                backgroundGradientFrom: "#fb8c00",
+                backgroundGradientTo: "#ffa726",
+                decimalPlaces: 2, // optional, defaults to 2dp
+                color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                style: {
+                    borderRadius: 16
+                },
+                propsForDots: {
+                    r: "6",
+                    strokeWidth: "2",
+                    stroke: "#ffa726"
+                }
+            }}
+            verticalLabelRotation={30}
+        />
+    );
+};
+
+export default TotalTaskTimeChart;
--- src/pages/Statistics/index.jsx
+++ src/pages/Statistics/index.jsx
@@ -1,61 +1,15 @@
-﻿import React, {useEffect, useState} from 'react';
+﻿import React from 'react';
 import Page from "../Page";
-import {Dimensions, View, Text} from "react-native";
-import {BarChart, LineChart} from "react-native-chart-kit";
+import {View} from "react-native";
 import {SelectList} from "react-native-dropdown-select-list/index";
-import {useDispatch, useSelector} from "react-redux";
+import {useSelector} from "react-redux";
 import MonthlyChart from "./MonthlyChart";
-import {fetchHalfYearTime, fetchTotalTasksTime} from "../../store/tasksSlice";
+import TotalTaskTimeChart from "./TotalTaskTimeChart";
 
-//extract this function to its own file
-function convertTimeStringToHours(timeString) {
-    // Split the time string into components
-    const [hours, minutes, seconds] = timeString.split(':');
-
-    // Parse hours, minutes, and seconds
-    const parsedHours = parseInt(hours, 10);
-    const parsedMinutes = parseInt(minutes, 10);
-    const parsedSeconds = parseFloat(seconds); // Parse seconds as float for decimal precision
-
-    // Calculate total hours
-    const totalHours = parsedHours + (parsedMinutes / 60) + (parsedSeconds / 3600);
-
-    return totalHours;
-}
 const Statistics = ({navigation}) => {
     
     const {tasks} = useSelector(state => state.tasks);
-    const dispatch = useDispatch();
-    const [data, setData] = useState({
-        labels: ["January", "February", "March", "April", "May"],
-        datasets: [
-            {
-                data: [20, 45, 28, 80, 99]
-            }
-        ]
-    });
-    const {totalTasksTime} = useSelector(state => state.tasks);
-
-    useEffect(() => {
-        dispatch(fetchTotalTasksTime());
-        //console.log(halfYearTime.map(month => month.time) );
-    }, [])
-
-    useEffect(() => {
-        const labels = totalTasksTime.map(task => task.name);
-        const data = totalTasksTime.map(task => convertTimeStringToHours(task.time));
-        const newData = {
-            labels: labels,
-            datasets: [
-                {
-                    data: data
-                }
-            ]
-        };
-        setData(newData);
-    }, [totalTasksTime]);
     
-
     return (
         <Page navigation={navigation}>
             <View style={{position: "absolute", top: 40, left: 30, width: 300, zIndex: 1}}>
@@ -66,30 +20,7 @@ const Statistics = ({navigation}) => {
                             setSelected={(val) => console.log(val)}/>
             </View>
             <MonthlyChart />
-            <BarChart
-               // style={{color: "blue"}}
-                data={data}
-                width={Dimensions.get("window").width}
-                height={220}
-                yAxisSuffix="h"
-                chartConfig={{
-                    backgroundColor: "#e26a00",
-                    backgroundGradientFrom: "#fb8c00",
-                    backgroundGradientTo: "#ffa726",
-                    decimalPlaces: 2, // optional, defaults to 2dp
-                    color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-                    labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-                    style: {
-                        borderRadius: 16
-                    },
-                    propsForDots: {
-                        r: "6",
-                        strokeWidth: "2",
-                        stroke: "#ffa726"
-                    }
-                }}
-                verticalLabelRotation={30}
-            />
+            <TotalTaskTimeChart />
         </Page>
     );
 };
