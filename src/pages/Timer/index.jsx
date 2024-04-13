7332725cfc33bb2c3ddf314888e25ea586700f49
teooko <teobogdann@yahoo.com>
2024-04-13 13:46:47 +0300

    Make both timers run at the same time

--- src/pages/Timer/index.jsx
+++ src/pages/Timer/index.jsx
@@ -1,4 +1,4 @@
-﻿import {View, Text, Dimensions, SectionList} from 'react-native';
+﻿import {View, Text, Dimensions, SectionList, StatusBar} from 'react-native';
 import Page from '../Page';
 import React, {useEffect, useState} from 'react';
 import {StyleSheet} from 'react-native';
@@ -9,11 +9,11 @@ import {icons} from '../../assets/Icons';
 import TimerControls from './TimerControls';
 import {useDispatch, useSelector} from 'react-redux';
 import {
+    openPicker,
     patchStopTimer,
     postStartTimer,
-    setCurrentTaskId,
+    setCurrentTaskId, setCurrentTime,
     setReset,
-    setTime,
     startTimer,
     stopTimer
 } from '../../store/timerSlice';
@@ -22,6 +22,7 @@ import TimerBubble from "./TimerBubble";
 import Picker from "./Picker";
 import useOrientation from "../../helpers/useOrientation";
 import {State} from "react-native-gesture-handler";
+import CountDown from "react-native-countdown-fixed";
 
 const {height, width} = Dimensions.get('window');
 function Timer({navigation}) {
@@ -85,44 +86,69 @@ function Timer({navigation}) {
     const {orientation} = useSelector(state => state.deviceInfo);
     useOrientation();
     useEffect(() => console.log("orientation changed"), [orientation]);
+
+    const [countDownId, setCountDownId] = useState(null);
+
+    useEffect(() => {
+        const id = new Date().getTime().toString()
+        setCountDownId(id)
+    }, [time, reset])
+
     return (
         <View>
-            
-            <Page navigation={navigation}>
-                <Picker />
-                <View style={{position: "absolute", top: 40, left: 30, width: 300, zIndex: 1}}>
-                    <SelectList dropdownItemStyles={{backgroundColor: "#B83838"}}
-                                boxStyles={{backgroundColor: "#B83838", borderWidth: 0}}
-                                dropdownStyles={{backgroundColor: "#B83838", borderColor: "#560D0D"}}
-                                data={tasks.map(task => ({ key: task.id, value: task.name }))}
-                                setSelected={(val) => dispatch(setCurrentTaskId(val))}/>
-                </View>
-                <Text style={{fontSize: 30, color: "white", marginTop: 70, marginLeft: "auto", marginRight: "auto"}}>
-                    Time to work
-                </Text>
-                <TimerBubble
-                    backWaveStyle={backWaveStyle}
-                    frontWaveStyle={frontWaveStyle}
-                    riseAnimationStyle={riseAnimationStyle}
+            <View style={{display: "flex", alignItems: "center", justifyContent: "center", backgroundColor: "black", height: "100%", width: "100%", position: "absolute", zIndex: orientation === "PORTRAIT" ? -2 : 3}}>
+                <StatusBar />
+                <CountDown
+                    id={countDownId}
+                    size={40}
+                    until={time}
+                    style={styles.timer}
+                    onFinish={() => handleStopTimer(currentWorkSessionId)}
+                    digitStyle={{width: 50}}
+                    digitTxtStyle={{color: 'white'}}
+                    separatorStyle={{color: "white"}}
+                    timeToShow={['H', 'M', 'S']}
+                    timeLabels={{m: null, s: null}}
+                    showSeparator
+                    running={timerRunning}
+                    onPress={() => dispatch(openPicker())}
+                    onChange={(value) => dispatch(setCurrentTime(value))}
                 />
-                <TimerControls
-                    svg={svg}
-                    handleReset={handleReset}
-                    handlePress={handlePress}
-                />
-            </Page>
+            </View>
+    <Page navigation={navigation}>
+        <Picker/>
+        <View style={{position: "absolute", top: 40, left: 30, width: 300, zIndex: 1}}>
+            <SelectList dropdownItemStyles={{backgroundColor: "#B83838"}}
+                        boxStyles={{backgroundColor: "#B83838", borderWidth: 0}}
+                        dropdownStyles={{backgroundColor: "#B83838", borderColor: "#560D0D"}}
+                        data={tasks.map(task => ({key: task.id, value: task.name}))}
+                        setSelected={(val) => dispatch(setCurrentTaskId(val))}/>
         </View>
-    );
+        <Text style={{fontSize: 30, color: "white", marginTop: 70, marginLeft: "auto", marginRight: "auto"}}>
+            Time to work
+        </Text>
+        <TimerBubble
+            backWaveStyle={backWaveStyle}
+            frontWaveStyle={frontWaveStyle}
+            riseAnimationStyle={riseAnimationStyle}
+        />
+        <TimerControls
+            svg={svg}
+            handleReset={handleReset}
+            handlePress={handlePress}
+        />
+    </Page>
+        </View>
+);
 }
 
 const styles = StyleSheet.create({
     timer: {
         zIndex: 2,
         fontSize: 60,
-        position: 'absolute',
-        marginLeft: width / 2 - 90,
-        marginTop: height / 2 - 60,
-        color: 'white',
+        marginLeft: "auto",
+        marginRight: "auto",
+        alignSelf: "center",
         width: 180,
     },
     icon: {
