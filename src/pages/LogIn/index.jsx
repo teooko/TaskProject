701ffae37ef1e94cbb063a9090c3e2120c3ab84a
teooko <teobogdann@yahoo.com>
2024-04-23 20:30:02 +0300

    Navigate to home page once bearer token is available

--- src/pages/LogIn/index.jsx
+++ src/pages/LogIn/index.jsx
@@ -3,18 +3,21 @@ import {Button, View} from "react-native";
 import NavigationBar from "../Page/NavigationBar";
 import {useDispatch, useSelector} from "react-redux";
 import {postLogInDefault} from "../../store/accountSlice";
+import {useNavigation} from "@react-navigation/native";
+import Home from "../Home";
 
-const LogIn = ({navigation}) => {
+const LogIn = ({nav}) => {
     const dispatch = useDispatch();
     const {bearerToken} = useSelector(state => state.account);
-    
+    const navigation = useNavigation();
     useEffect(() => {
-        
+        if(bearerToken !== null)
+            navigation.navigate(Home);
     }, [bearerToken]);
     
     return (
         <View>
-            <NavigationBar navigation={navigation} />
+            <NavigationBar navigation={nav} />
             <Button title={"Log in"} onPress={() => dispatch(postLogInDefault())}></Button>
         </View>
     );
