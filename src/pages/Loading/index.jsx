a0ac9c267030558b22fbe7cc41770fcd6eedc11d
teooko <teobogdann@yahoo.com>
2024-05-05 23:19:49 +0300

    Fix fetching tasks

--- src/pages/Loading/index.jsx
+++ src/pages/Loading/index.jsx
@@ -15,7 +15,7 @@ import Animated, {
     withTiming
 } from "react-native-reanimated";
 import {fetchWeeklyTasks, insertDays, resetCalendarState, selectDay} from "../../store/slice";
-import {fetchDailyTasks, fetchTasks} from "../../store/tasksSlice";
+import {fetchDailyTasks, fetchTasks, resetTaskState} from "../../store/tasksSlice";
 import {useDispatch, useSelector} from "react-redux";
 import Home from "../Home";
 import {resetBearerToken} from "../../store/accountSlice";
@@ -47,12 +47,16 @@ const Loading = ({navigation}) => {
     
     useEffect(() => {
         if(isFocused) {
+            dispatch(resetBearerToken());
+            dispatch(resetTaskState());
             dispatch(resetCalendarState());
+            
             const currentDate = new Date();
             const year = currentDate.getFullYear();
             const month = String(currentDate.getMonth() + 1).padStart(2, '0');
             const day = String(currentDate.getDate()).padStart(2, '0');
             const formattedDate = `${year}-${month}-${day}`;
+            dispatch(fetchTasks(bearerToken));
             dispatch(selectDay(0));
             dispatch(fetchDailyTasks({bearerToken, date: formattedDate}));
             dispatch(insertDays());
