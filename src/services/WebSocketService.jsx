b183497867a9ecd922abcf44eecacde8e38cd345
teooko <teobogdann@yahoo.com>
2024-05-20 19:25:37 +0300

    Temporarily fix web socket wrapper errors

--- src/services/WebSocketService.jsx
+++ src/services/WebSocketService.jsx
@@ -1,9 +1,9 @@
 ﻿import {useEffect, useState} from "react";
 import React from 'react';
-import {Alert, Modal, Pressable, View} from "react-native";
+import {Alert, Modal, Pressable, View, StyleSheet, Text} from "react-native";
 import {useSelector} from "react-redux";
 
-const [ws, setWs] = useState(null);
+/*
 const [invitation, setInvitation] = useState(null);
 
 const {username, bearerToken} = useSelector(state => state.account);
@@ -56,11 +56,12 @@ const receiveInvitation = (ws) => {
             });
         }
     }
-}
+}*/
 const WebSocketService = ({children}) => {
+    const [ws, setWs] = useState(null);
     const [modalVisible, setModalVisible] = useState(false);
     return (
-        <View>
+        <View style={styles.wrapper}>
             <Modal
                 animationType="slide"
                 transparent={true}
@@ -84,6 +85,54 @@ const WebSocketService = ({children}) => {
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    centeredView: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 22,
+    },
+    modalView: {
+        margin: 20,
+        backgroundColor: 'white',
+        borderRadius: 20,
+        padding: 35,
+        alignItems: 'center',
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+    },
+    button: {
+        borderRadius: 20,
+        padding: 10,
+        elevation: 2,
+    },
+    buttonOpen: {
+        backgroundColor: '#F194FF',
+    },
+    buttonClose: {
+        backgroundColor: '#2196F3',
+    },
+    textStyle: {
+        color: 'white',
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    modalText: {
+        marginBottom: 15,
+        textAlign: 'center',
+    },
+    wrapper: {
+        width: "100%",
+        height: "100%"
+    }
+});
 
 export default WebSocketService;
 
