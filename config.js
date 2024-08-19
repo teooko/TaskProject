bf1ff2bba2bec35a6507b051c3dcfbc17da6e4e8
teooko <teobogdann@yahoo.com>
2024-08-19 19:34:45 +0300

    Create a test debounced timer

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-﻿export const API_DOMAIN = 'http://192.168.1.104:5133';
-export const WEB_SOCKET_URL= 'ws://192.168.1.104:8080';
+﻿export const API_DOMAIN = 'http://192.168.0.119:5133';
+export const WEB_SOCKET_URL= 'ws://192.168.0.119:8080';
--- src/pages/Test/index.jsx
+++ src/pages/Test/index.jsx
@@ -1,9 +1,54 @@
-﻿import {Text} from "react-native";
+﻿import {Pressable, Text, View} from "react-native";
+import CountDown from "react-native-countdown-fixed";
+import {openPicker, setCurrentTime, startTimer, stopTimer} from "../../store/timerSlice";
+import React, {useCallback, useEffect, useRef, useState} from "react";
+import {SvgXml} from "react-native-svg";
+import {icons} from "../../assets/Icons";
 const Test = () => {
-    return ( 
-    <Text>
-        se incarca
-    </Text>)
+    const id = React.useRef(new Date().getTime().toString()).current;
+
+    const [timerRunning, setTimerRunning] = useState(false);
+    const [pressed, setPressed] = useState(false);
+
+    const timerRef = useRef(null); // Use useRef to persist timer ID
+
+    const toggleTimer = () => {
+        setPressed((state) => !state);
+        if (timerRef.current) {
+            clearTimeout(timerRef.current);
+        }
+        if(timerRunning)
+            setTimerRunning(false);
+        else
+            timerRef.current = setTimeout(() => {
+                setTimerRunning(prevState => !prevState);
+                setPressed(!timerRunning);
+            }, 500);
+    };
+
+    const {start, pause} = icons;
+    return (
+        <View>
+            <CountDown
+                id={id}
+                size={40}
+                until={100}
+                //style={styles.timer}
+                onFinish={() => console.log("finished")}
+                digitStyle={{width: 50}}
+                digitTxtStyle={{color: 'black'}}
+                separatorStyle={{color: "black"}}
+                timeToShow={['H', 'M', 'S']}
+                timeLabels={{m: null, s: null}}
+                showSeparator
+                running={timerRunning}
+                onPress={() => console.log("pressed")}
+                onChange={(value) => console.log(value)}
+            />
+            <Pressable style={{alignSelf: "center"}} onPress={toggleTimer}>
+                <SvgXml xml={pressed ? pause : start} width={50} height={50}/>
+            </Pressable>
+        </View>)
 }
 
 export default Test;
